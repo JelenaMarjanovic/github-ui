a4afab8788c2f95140b0646ef9f55a1abbac2452
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-26 03:23:43 +0200

    Exercise #2 - Add dynamic content to orgs

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,26 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {
+        id: "emberjs",
+      },
+      {
+        id: "ember-cli",
+      },
+      {
+        id: "microsoft",
+      },
+      {
+        id: "yahoo",
+      },
+      {
+        id: "netflix",
+      },
+      {
+        id: "facebook",
+      },
+    ];
+  },
 });
