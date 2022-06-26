d66c530a2a3fbc832c77e9ea7e6019089fc15cb5
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-26 02:59:33 +0200

    Exercise #2 - Set up redirects

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,9 @@
 import Ember from "ember";
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+  beforeModel() {
+    this._super(...arguments);
+
+    this.replaceWith("orgs");
+  },
+});
