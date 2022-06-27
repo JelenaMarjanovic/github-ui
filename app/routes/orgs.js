497e35c2920a6cea3c83ee8de084c380479e187a
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-27 12:23:28 +0200

    Exercise #3 - Add and bind action for click event

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -2,7 +2,11 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   favorites: Ember.inject.service(),
-
+  actions: {
+    favoriteClicked(org) {
+      this.get("favorites").favoriteItem(org);
+    },
+  },
   model() {
     return [
       {
--- app/services/favorites.js
+++ app/services/favorites.js
@@ -1,5 +1,16 @@
+/* eslint-disable no-console */
 import Ember from "ember";
 
 export default Ember.Service.extend({
   items: [],
+
+  favoriteItem(item) {
+    this.get("items").addObject(item);
+
+    console.log(
+      this.get("items")
+        .map((item) => item.id)
+        .join(", ")
+    );
+  },
 });
