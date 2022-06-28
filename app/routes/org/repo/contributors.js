0d8dc1b4761440e8661b019a53d2b35a64de54e8
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-28 11:46:59 +0200

    Exercise #4 - Replace hard-coded with real data (contributors)

--- app/routes/org/repo/contributors.js
+++ app/routes/org/repo/contributors.js
@@ -1,4 +1,20 @@
-import Ember from 'ember';
+import Ember from "ember";
+import $ from "jquery";
 
 export default Ember.Route.extend({
+  model() {
+    let orgId = Ember.get(this.modelFor("org"), "login");
+    let repoId = Ember.get(this.modelFor("org.repo"), "name");
+
+    return $.get(
+      `https://api.github.com/repos/${orgId}/${repoId}/contributors`
+    ).then((rawContributors) => {
+      return rawContributors.map((rawContributor) => {
+        rawContributor.oldId = rawContributor.id;
+        rawContributor.id = rawContributor.login;
+
+        return rawContributor;
+      });
+    });
+  },
 });
