49ccb0e99dc2f4a07fd22d99db648c6c9cc6b5f6
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-28 09:57:54 +0200

    Exercise #4 - Replace hard-coded with real data (org)

--- app/routes/org.js
+++ app/routes/org.js
@@ -1,4 +1,17 @@
-import Ember from 'ember';
+import Ember from "ember";
+import $ from "jquery";
 
 export default Ember.Route.extend({
+  model(params) {
+    return $.get(`https://api.github.com/orgs/${params.orgId}`).then(
+      (rawOrg) => {
+        // Backing up GitHub's numeric ID
+        rawOrg.oldId = rawOrg.id;
+        // Use the name of the repo as our ID
+        rawOrg.id = rawOrg.name;
+
+        return rawOrg;
+      }
+    );
+  },
 });
