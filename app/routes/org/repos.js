75a95e2fc545a96c2ffcd7bde7c634c6ae1245bf
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-28 09:58:08 +0200

    Exercise #4 - Replace hard-coded with real data (repos)

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -1,4 +1,20 @@
-import Ember from 'ember';
+import Ember from "ember";
+import $ from "jquery";
 
 export default Ember.Route.extend({
+  // eslint-disable-next-line no-unused-vars
+  model(params) {
+    let orgId = Ember.get(this.modelFor("org"), "id");
+
+    return $.get(`https://api.github.com/orgs/${orgId}/repos`).then(
+      (rawRepos) => {
+        return rawRepos.map((rawRepo) => {
+          rawRepo.oldId = rawRepo.id;
+          rawRepo.id = rawRepo.name;
+
+          return rawRepo;
+        });
+      }
+    );
+  },
 });
