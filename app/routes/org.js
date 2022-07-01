1ff6f9aaf87bc9cb6bf78871137728cbb7e6c9f7
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-07-01 17:38:54 +0200

    Exercise #5 - Remove timeout

--- app/routes/org.js
+++ app/routes/org.js
@@ -3,22 +3,15 @@ import $ from "jquery";
 
 export default Ember.Route.extend({
   model(params) {
-    return $.get(`https://api.github.com/orgs/${params.orgId}`)
-      .then((rawOrg) => {
+    return $.get(`https://api.github.com/orgs/${params.orgId}`).then(
+      (rawOrg) => {
         // Backing up GitHub's numeric ID
         rawOrg.oldId = rawOrg.id;
         // Use the name of the repo as our ID
         rawOrg.id = rawOrg.name;
 
         return rawOrg;
-      })
-      .then(function (data) {
-        // eslint-disable-next-line no-unused-vars
-        return new Ember.RSVP.Promise((res, rej) => {
-          Ember.run.later(() => {
-            res(data);
-          }, 2000);
-        });
-      });
+      }
+    );
   },
 });
