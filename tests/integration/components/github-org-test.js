c6d1b1686e80dee1d174bc8ad9b6fbd4047f7a52
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-07-20 03:35:52 +0200

    Exercise #6 - Replace each body on /orgs page with github-org component

---
+++ tests/integration/components/github-org-test.js
@@ -0,0 +1,15 @@
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
+
+moduleForComponent("github-org", "Integration | Component | github org", {
+  integration: true,
+});
+
+test("it renders", function (assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{github-org}}`);
+
+  assert.equal(this.$().text().trim(), "[ Favorite ]");
+});
