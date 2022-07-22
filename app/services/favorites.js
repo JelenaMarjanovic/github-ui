752e2768be9efbfd6324eaa624196aa64f66c382
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-07-22 02:54:48 +0200

    Exercise #7 - Alternate text of (Un)Favorite button and add/remove items

--- app/services/favorites.js
+++ app/services/favorites.js
@@ -4,13 +4,20 @@ import Ember from "ember";
 export default Ember.Service.extend({
   items: [],
 
-  favoriteItem(item) {
-    this.get("items").addObject(item);
-
+  log() {
     console.log(
       this.get("items")
         .map((item) => item.id)
         .join(", ")
     );
+  },
+
+  favoriteItem(item) {
+    this.get("items").addObject(item);
+    this.log();
+  },
+  unfavoriteItem(item) {
+    this.get("items").removeObject(item);
+    this.log();
   },
 });
