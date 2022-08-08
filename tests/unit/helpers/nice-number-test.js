a2ab0864306f6b8fa657f9659f69a58a5ef70030
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-08-08 03:31:54 +0200

    Exercise #9 - Add unit test for helper nice-number (TDD approach)

---
+++ tests/unit/helpers/nice-number-test.js
@@ -0,0 +1,13 @@
+import { niceNumber } from "github-ui/helpers/nice-number";
+import { module, test } from "qunit";
+
+module("Unit | Helper | nice number");
+
+// Replace this with your real tests.
+test("it works", function (assert) {
+  assert.expect(3);
+
+  assert.equal(niceNumber(), "", "Empty");
+  assert.equal(niceNumber([42]), "42", "A number < 1000");
+  assert.equal(niceNumber([4199]), "4.2K", "A number > 1000");
+});
